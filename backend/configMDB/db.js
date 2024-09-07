a4bc869ac355045bd4a3b3c2a790f4fa55ad09f5
SatwikPanda <tony943838@gmail.com>
2024-09-07 18:08:43 +0530

    Added routes

--- backend/configMDB/db.js
+++ backend/configMDB/db.js
@@ -6,7 +6,7 @@ dotenv.config();
 export const connectDB = async () => {
     try {
         const conn = mongoose.connect(process.env.MONGO_URI);
-        console.log(`MongoDB Connected: ${conn.connection.host}`);
+        console.log("MongoDB Connected");
     }
     catch(error) {
         console.log(`Error: ${error.message}`);
