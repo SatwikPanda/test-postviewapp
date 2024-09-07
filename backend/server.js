7f45c84195e49acdc58e5d76b0b5652cbadd1bd6
SatwikPanda <tony943838@gmail.com>
2024-09-07 11:04:43 +0530

    Shifted to mongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,16 @@
 import express from 'express';
+import dotenv from "dotenv";
+import { connectDB } from "./configMDB/db.js";
+
+dotenv.config();
 
 const app = express();
 
-
 app.get('/details', (req, res) => {
-
-    //Create a database that from which it will fetch user details.
     res.send("Server is Ready");
 })
 
 app.listen(5000, () => {
+    connectDB();
     console.log('Server listening at http://localhost:5000');
 });
