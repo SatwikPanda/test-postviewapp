ee500450f7b1fa1bb2c3f950a8094f1b8f5d4f75
SatwikPanda <tony943838@gmail.com>
2024-09-07 18:32:09 +0530

    Backend Complete

---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,57 @@
+import User from "../models/user.model.js";
+import mongoose from "mongoose";
+
+export const getUser = async (req, res) => {
+    try {
+      const users = await User.find({});
+      res.status(200).json({ success: true, data: users });
+    } 
+    catch (error) {
+      console.log(`Error: ${error.message}`);
+      res.status(500).json({ success: false, message: "Internal Server Error" });
+    }
+}
+
+export const createUser = async (req, res) => {
+    const details = req.body;
+    if(!details.name || !details.email || !details.photoURL) {
+        return res.status(400).json({ success:false, message: "All the fields are required" });
+    }
+
+    const newUser = new User(details);
+
+    try {
+        await newUser.save();
+        res.status(201).json({ success: true, message: "User created successfully" });
+    } catch (error) {
+        console.log(`Error: ${error.message}`);
+        res.status(500).json({ success: false, message: "Internal Server Error" });
+    }
+}
+
+export const updateUser = async (req, res) => {
+    const { id } = req.params;
+    const details = req.body;
+
+    if(!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ success: false, message: "User not found" });
+    }
+
+
+    try {
+        const updatedUser = await User.findByIdAndUpdate(id, details, {new:true});
+        res.status(200).json({ success: true, data: updatedUser });
+    } catch (error) {
+        res.status(500).json({ success: false, message: "Internal Error" });
+    }
+}
+
+export const deleteUser = async (req, res) => {
+    const { id } = req.params;
+    try {
+        await User.findByIdAndDelete(id);
+        res.status(200).json({ success: true, message: "User deleted successfully" });
+    } catch (error) {
+        res.status(404).json({ success: false, message: "User not found" });
+    }
+}
--- backend/server.js
+++ backend/server.js
@@ -1,68 +1,15 @@
 import express from 'express';
 import dotenv from "dotenv";
 import { connectDB } from "./configMDB/db.js";
-import User from './models/user.model.js';
+import userRoutes from "./routes/user.route.js";
 
 dotenv.config();
 
 const app = express();
 app.use(express.json()); //allows use to accept json data
+app.use("/api/users", userRoutes)
 
 
-app.get("/api/users", async (req, res) => {
-  try {
-    const users = await User.find({});
-    res.status(200).json({ success: true, data: users });
-  } 
-  catch (error) {
-    console.log(`Error: ${error.message}`);
-    res.status(500).json({ success: false, message: "Internal Server Error" });
-  }
-})
-
-app.post("/api/users", async (req, res) => {
-    const details = req.body;
-    if(!details.name || !details.email || !details.photoURL) {
-        return res.status(400).json({ success:false, message: "All the fields are required" });
-    }
-
-    const newUser = new User(details);
-
-    try {
-        await newUser.save();
-        res.status(201).json({ success: true, message: "User created successfully" });
-    } catch (error) {
-        console.log(`Error: ${error.message}`);
-        res.status(500).json({ success: false, message: "Internal Server Error" });
-    }
-});
-
-app.put("/api/users/:id", async (req, res) => {
-    const { id } = req.params;
-    const details = req.body;
-
-    if(!mongoose.Types.ObjectId.isValid(id)) {
-        return res.status(404).json({ success: false, message: "User not found" });
-    }
-
-
-    try {
-        const updatedUser = await User.findByIdAndUpdate(id, details, {new:true});
-        res.status(200).json({ success: true, data: updatedUser });
-    } catch (error) {
-        res.status(500).json({ success: false, message: "Internal Error" });
-    }
-});
-
-app.delete("/api/users/:id", async (req, res) => {
-    const { id } = req.params;
-    try {
-        await User.findByIdAndDelete(id);
-        res.status(200).json({ success: true, message: "User deleted successfully" });
-    } catch (error) {
-        res.status(404).json({ success: false, message: "User not found" });
-    }
-})
 
 app.listen(5000, () => {
     connectDB();
